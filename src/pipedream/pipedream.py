6e1bc8dbe5eee8f30b5e14b8caadb4726d53af35
Jay Vercellone <jay@pipedream.com>
2025-07-07 14:29:28 -0700

    Update custom client to match TS

--- src/pipedream/pipedream.py
+++ src/pipedream/pipedream.py
@@ -1,5 +1,8 @@
 import os
-from string import Template
+from typing import (
+    Literal,
+    Optional,
+)
 
 from .client import (
     AsyncClient,
@@ -8,37 +11,72 @@ from .client import (
 from .environment import PipedreamEnvironment
 
 
-class Pipedream(Client):
+class OAuthCredentials:
+
     def __init__(
         self,
-        project_id: str,
-        environment: PipedreamEnvironment = PipedreamEnvironment.PROD,
-        *args,
+        client_id: Optional[str] = None,
+        client_secret: Optional[str] = None,
+    ):
+        self.client_id = client_id or os.getenv("PIPEDREAM_CLIENT_ID")
+        self.client_secret = client_secret or os.getenv(
+            "PIPEDREAM_CLIENT_SECRET")
+
+        if not self.client_id or not self.client_secret:
+            raise ValueError("OAuth client ID and secret are required")
+
+
+class Pipedream(Client):
+
+    def __init__(
+        self,
+        *,
+        credentials: OAuthCredentials = OAuthCredentials(),
+        project_id: Optional[str] = None,
+        environment: Literal["production", "development"] = "production",
+        api_environment: PipedreamEnvironment = PipedreamEnvironment.PROD,
         **kwargs,
     ):
-        super().__init__(base_url=_get_base_url(environment), *args, **kwargs)
-        self.project_id = project_id
+        project_id = project_id or os.getenv("PIPEDREAM_PROJECT_ID")
+        if not project_id:
+            raise ValueError("Project ID is required")
+
+        if not credentials.client_id or not credentials.client_secret:
+            raise ValueError("OAuth client ID and secret are required")
+
+        super().__init__(
+            client_id=credentials.client_id,
+            client_secret=credentials.client_secret,
+            environment=api_environment,
+            project_id=project_id,
+            x_pd_environment=environment,
+            **kwargs,
+        )
 
 
 class AsyncPipedream(AsyncClient):
+
     def __init__(
         self,
-        project_id: str,
-        environment: PipedreamEnvironment = PipedreamEnvironment.PROD,
-        *args,
+        *,
+        credentials: OAuthCredentials = OAuthCredentials(),
+        project_id: Optional[str] = None,
+        environment: Literal["production", "development"] = "production",
+        api_environment: PipedreamEnvironment = PipedreamEnvironment.PROD,
         **kwargs,
     ):
-        super().__init__(base_url=_get_base_url(environment), *args, **kwargs)
-        self.project_id = project_id
+        project_id = project_id or os.getenv("PIPEDREAM_PROJECT_ID")
+        if not project_id:
+            raise ValueError("Project ID is required")
 
+        if not credentials.client_id or not credentials.client_secret:
+            raise ValueError("OAuth client ID and secret are required")
 
-def _get_base_url(environment: PipedreamEnvironment) -> str:
-    if not environment:
-        raise Exception("Please pass environment to construct the client")
-
-    user = os.getenv("DEV_NAMESPACE", "")
-    return Template(environment.value).substitute(
-        {
-            "user": user,
-        }
-    )
+        super().__init__(
+            client_id=credentials.client_id,
+            client_secret=credentials.client_secret,
+            environment=api_environment,
+            project_id=project_id,
+            x_pd_environment=environment,
+            **kwargs,
+        )
