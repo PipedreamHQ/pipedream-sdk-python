cfe4b1b6ff3922165a7cd365a83c8a979c541659
Jay Vercellone <jay@pipedream.com>
2025-07-17 11:58:42 -0700

    Client init improvements
    * Source `PIPEDREAM_*` env variables for config data such as project
    ID, environment, credentials, etc.
    * Add support for static access tokens

--- src/pipedream/pipedream.py
+++ src/pipedream/pipedream.py
@@ -14,17 +14,21 @@ class Pipedream(Client):
     def __init__(
         self,
         *,
-        client_id: Optional[str] = None,
-        client_secret: Optional[str] = None,
-        project_id: Optional[str] = None,
-        project_environment: ProjectEnvironment = "production",
+        access_token: Optional[str] = os.getenv("PIPEDREAM_ACCESS_TOKEN"),
+        client_id: Optional[str] = os.getenv("PIPEDREAM_CLIENT_ID"),
+        client_secret: Optional[str] = os.getenv("PIPEDREAM_CLIENT_SECRET"),
+        project_id: Optional[str] = os.getenv("PIPEDREAM_PROJECT_ID"),
+        project_environment: ProjectEnvironment = os.getenv(
+            "PIPEDREAM_PROJECT_ENVIRONMENT",
+            "production",
+        ),
         environment: PipedreamEnvironment = PipedreamEnvironment.PROD,
     ):
-        project_id = project_id or os.getenv("PIPEDREAM_PROJECT_ID")
         if not project_id:
             raise ValueError("Project ID is required")
 
         super().__init__(
+            _token_getter_override=_new_token_getter(access_token),
             base_url=_get_base_url(environment),
             client_id=client_id,
             client_secret=client_secret,
@@ -38,24 +42,27 @@ class AsyncPipedream(AsyncClient):
     def __init__(
         self,
         *,
-        client_id: Optional[str] = None,
-        client_secret: Optional[str] = None,
-        project_id: Optional[str] = None,
-        project_environment: ProjectEnvironment = "production",
+        access_token: Optional[str] = os.getenv("PIPEDREAM_ACCESS_TOKEN"),
+        client_id: Optional[str] = os.getenv("PIPEDREAM_CLIENT_ID"),
+        client_secret: Optional[str] = os.getenv("PIPEDREAM_CLIENT_SECRET"),
+        project_id: Optional[str] = os.getenv("PIPEDREAM_PROJECT_ID"),
+        project_environment: ProjectEnvironment = os.getenv(
+            "PIPEDREAM_PROJECT_ENVIRONMENT",
+            "production",
+        ),
         environment: PipedreamEnvironment = PipedreamEnvironment.PROD,
-        **kwargs,
     ):
-        project_id = project_id or os.getenv("PIPEDREAM_PROJECT_ID")
+        project_id = project_id
         if not project_id:
             raise ValueError("Project ID is required")
 
         super().__init__(
+            _token_getter_override=_new_token_getter(access_token),
             base_url=_get_base_url(environment),
             client_id=client_id,
             client_secret=client_secret,
             project_id=project_id,
             project_environment=project_environment,
-            **kwargs,
         )
 
 
@@ -64,3 +71,10 @@ def _get_base_url(environment: PipedreamEnvironment) -> str:
     Returns the base URL for the given environment.
     """
     return os.path.expandvars(environment.value)
+
+
+def _new_token_getter(access_token: Optional[str] = None):
+    """
+    Returns a new token getter function that retrieves the access token.
+    """
+    return (lambda: access_token) if access_token else None
