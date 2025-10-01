c41d5e985aa1acf9df2b2b88b8bde9d8df3ac2e2
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-10-01 19:00:42 +0000

    SDK regeneration

--- src/pipedream/types/configurable_prop_object.py
+++ src/pipedream/types/configurable_prop_object.py
@@ -9,7 +9,7 @@ from ..core.serialization import FieldMetadata
 
 
 class ConfigurablePropObject(UniversalBaseModel):
-    type: typing.Literal["object"] = "object"
+    type: typing.Optional[typing.Literal["object"]] = None
     name: str = pydantic.Field()
     """
     When building `configuredProps`, make sure to use this field as the key when setting the prop value
